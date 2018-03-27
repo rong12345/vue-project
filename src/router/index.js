8072fdab95d023fb0785d893c142e9a18a960a29
rong12345 <1165895564@qq.com>
2018-03-27 17:21:43 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import VueResource from 'vue-resource'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import IndexPage from '@/pages/index'
+import DetailPage from '@/pages/detail'
+import Analysis from '@/pages/Detail/analysis'
+import Count from '@/pages/Detail/count'
+import Forecast from '@/pages/Detail/forecast'
+import Publish from '@/pages/Detail/publish'
 
 Vue.use(Router)
 Vue.use(VueResource)
@@ -13,6 +18,29 @@ export default new Router({
     {
       path: '/',
       component: IndexPage
+    },
+    {
+      path: '/detail',
+      component: DetailPage,
+      redirect: '/detail/analysis',
+      children:[
+        {
+          path:'forecast',
+          component:Forecast
+        },
+        {
+          path:'analysis',
+          component:Analysis
+        },
+        {
+          path:'publish',
+          component:Publish
+        },
+        {
+          path:'count',
+          component:Count
+        }
+      ]
     }
   ]
 })
